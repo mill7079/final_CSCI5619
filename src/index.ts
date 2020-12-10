d8e9f3c4519ebb9276ba0ee5e7f2d5dd3982526f
Allison Miller <mill7079@umn.edu>
2020-12-09 23:12:55 -0700

    Add parts of message passing - Something's screwy but I've been staring at it for too long and I need   a break

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
 import { Vector3, Color3 } from "@babylonjs/core/Maths/math";
 import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { WebXRControllerComponent } from "@babylonjs/core/XR/motionController/webXRControllercomponent";
 import { WebXRInputSource } from "@babylonjs/core/XR/webXRInputSource";
 import { WebXRCamera } from "@babylonjs/core/XR/webXRCamera";
 import { PointLight } from "@babylonjs/core/Lights/pointLight";
@@ -18,19 +19,30 @@ import { InputPassword, InputText } from "@babylonjs/gui/2D/controls";
 import { VirtualKeyboard } from "@babylonjs/gui/2D/controls/virtualKeyboard";
 import { TextBlock } from "@babylonjs/gui/2D/controls/textBlock";
 import { SceneSerializer } from "@babylonjs/core/Misc/sceneSerializer";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { Mesh } from "@babylonjs/core/Meshes/mesh";
+
 import * as MATRIX from "matrix-js-sdk";
 
 // Side effects
 import "@babylonjs/core/Helpers/sceneHelpers";
 import "@babylonjs/inspector";
-import { Mesh } from "@babylonjs/core/Meshes/mesh";
 
+// handle message sending, given a JSON string representation
 module Messages {
     export var client = MATRIX.createClient("https://matrix.org");
+    export var room = "!FQlzwKdCBFuEnQusdk:matrix.org";
 
-    //export function client() {
-    //    return client;
-    //}
+    export function sendMessage(isNotice: boolean, content: string) {
+        console.log("send message");
+        let send = {
+            body: content,
+            msgtype: isNotice ? "m.notice" : "m.text"
+        };
+        client.sendEvent(room, "m.room.message", send, "", (err: any, res: any) => {
+            console.log(err);
+        });
+    }
 }
 
 class Game 
@@ -47,14 +59,16 @@ class Game
 
     private client: any;
     private user = "";
-    private room = "!FQlzwKdCBFuEnQusdk:matrix.org";
+    private room = Messages.room;
 
     private guiPlane: AbstractMesh | null;
     private loginStatus: AbstractMesh | null;
     private black = "#070707";
     private gray = "#808080";
 
-    private gameState: State;
+    //private gameState: State;
+    private envUsers: Map<string, User>;
+    private envObjects: Map<string, Mesh>;
 
     private userPosition: Vector3 | null; 
     private leftPosition: Vector3 | null;
@@ -97,10 +111,9 @@ class Game
         this.loginStatus = null;
         this.failedLogin = null;
 
-        // debugging
-        //console.log("domain " + this.client.getHomeserverUrl());
-
-        this.gameState = new State();
+        //this.gameState = new State();
+        this.envUsers = new Map();
+        this.envObjects = new Map();
     }
 
     start() : void 
@@ -319,9 +332,14 @@ class Game
         //console.log("textvec2: " + textVec2._x);
         //console.log("keys: " + Object.keys(textVec2));
 
-        //var cube = MeshBuilder.CreateBox("cube", { size: 1 }, this.scene);
-        //console.log("stringify cube: " + JSON.stringify(BABYLON.SceneSerializer.SerializeMesh(cube)));
-        //console.log("unstringify cube: " + JSON.parse(JSON.stringify(BABYLON.SceneSerializer.SerializeMesh(cube))).name);
+        var cube = MeshBuilder.CreateBox("cube", { size: 1 }, this.scene);
+        cube.position = new Vector3(1, 8, 1);
+        var stringified = JSON.stringify(SceneSerializer.SerializeMesh(cube));
+        //console.log("stringify cube: " + JSON.stringify(SceneSerializer.SerializeMesh(cube)));
+        SceneLoader.ImportMesh("", "", ("data:" + stringified), this.scene);
+        var cubeParse = JSON.parse(JSON.stringify(SceneSerializer.SerializeMesh(cube)));
+        console.log(Object.getOwnPropertyNames(cubeParse));
+
 
         //var obj = {
         //    propA: 1,
@@ -329,11 +347,14 @@ class Game
         //    propC: new Vector3(4, 5, 6)
         //};
 
-        //console.log("obj: " + obj);
+        ////console.log("obj: " + obj);
         //var pso = JSON.parse(JSON.stringify(obj));
         //console.log("stringify obj: " + JSON.stringify(obj));
         //console.log("parse string obj: " + JSON.parse(JSON.stringify(obj)));
-        //console.log("prop A + 1: " + ((+pso.propA) + 1));
+        ////console.log("prop A + 1: " + ((+pso.propA) + 1));
+
+        //var x: Vector3 = Object.assign(new Vector3(), pso.propC);
+        //console.log("obj assign to vector: " + (x instanceof Vector3));
 
 
 
@@ -351,38 +372,91 @@ class Game
     // The main update loop will be executed once per frame before the scene is rendered
     private update() : void
     {
-        if (this.xrCamera){
-        this.userPosition = this.xrCamera!.globalPosition.clone(); 
-        }
-        if (this.rightController){
-        this.rightPosition = this.rightController!.pointer.position.clone(); 
-        }
-        if (this.leftController){
-        this.leftPosition = this.leftController!.pointer.position.clone(); 
-        }
+        //if (this.xrCamera){
+        //this.userPosition = this.xrCamera!.globalPosition.clone(); 
+        //}
+        //if (this.rightController){
+        //this.rightPosition = this.rightController!.pointer.position.clone(); 
+        //}
+        //if (this.leftController){
+        //this.leftPosition = this.leftController!.pointer.position.clone(); 
+        //}
 
+        this.processControllerInput();
+    }
+
+    private processControllerInput() {
+        this.onRightSqueeze(this.rightController?.motionController?.getComponent("xr-standard-squeeze"));
+    }
+
+    private onRightSqueeze(component?: WebXRControllerComponent) {
+        if (component?.changes.pressed) {
+            if (component?.pressed) {
+                var newMesh = MeshBuilder.CreateBox("cube", { size: 1 }, this.scene);
+                newMesh.position = new Vector3(2, 3, 4);
+                this.envObjects.set(newMesh.name, newMesh);
+
+                var message = {
+                    status: "create",
+                    type: "box",
+                    id: newMesh.name,
+                    info: {
+                        position: newMesh.position,
+                        rotation: newMesh.rotation,
+                        opts: {
+                            size: 1
+                        }
+                    }
+                };
+
+                Messages.sendMessage(false, JSON.stringify(message));
+            }
+        }
     }
 
     // updates environment according to message received from room
     // creates a cube if the message was 'cube' and a sphere if message was 'sphere'
     // mostly just testing things at this point, the real thing is going to be way more complicated
     private updateEnv(message: string) {
-
-        //console.log("update found: " + message);
-        if (message == "cube") {
-            console.log("create a cube!");
-            var cube = MeshBuilder.CreateBox("cube", { "size": 1 }, this.scene);
-            cube.position = new Vector3(3, 1.5, 0);
-        } else if (message == "sphere") {
-            console.log("create a sphere!");
-            var sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this.scene);
-            sphere.position = new Vector3(0, 1.5, -3);
+        console.log("message: " + message);
+        var msg = JSON.parse(message);
+        var msgInfo = JSON.parse(msg.info);
+        switch (msg.status) {
+            case "create":
+                switch (msg.type) {
+                    case "box":
+                        var newMesh = MeshBuilder.CreateBox(msg.id, JSON.parse(msgInfo.opts), this.scene);
+                        newMesh.position = Object.assign(newMesh.position, msgInfo.position);
+                        newMesh.rotation = Object.assign(newMesh.rotation, msgInfo.rotation);
+                        this.envObjects.set(msg.id, newMesh);
+                        break;
+                    case "sphere":
+                        break;
+                    default:
+                        console.log("shape not matched");
+                }
+                break;
+            case "update":
+                break;
+            case "remove":
+                break;
         }
 
-        else if (message.startsWith("user")){
-            console.log('this is the message that will be used to update environment'); 
-            console.log(message); 
-        }
+        ////console.log("update found: " + message);
+        //if (message == "cube") {
+        //    console.log("create a cube!");
+        //    var cube = MeshBuilder.CreateBox("cube", { "size": 1 }, this.scene);
+        //    cube.position = new Vector3(3, 1.5, 0);
+        //} else if (message == "sphere") {
+        //    console.log("create a sphere!");
+        //    var sphere = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this.scene);
+        //    sphere.position = new Vector3(0, 1.5, -3);
+        //}
+
+        //else if (message.startsWith("user")){
+        //    console.log('this is the message that will be used to update environment'); 
+        //    console.log(message); 
+        //}
     }
 
     private async connect(user: string, pass: string) {
@@ -390,19 +464,19 @@ class Game
         await this.client.login("m.login.password", { user: user, password: pass }).then((response: any) => {
             console.log("logged in!");
 
-            var user_array = []
-            user_array.push(this.leftPosition);
-            user_array.push(this.rightPosition); 
-            user_array.push(this.userPosition); 
+            //var user_array = []
+            //user_array.push(this.leftPosition);
+            //user_array.push(this.rightPosition); 
+            //user_array.push(this.userPosition); 
 
             // add user info to chat to update other info 
-            const user_info = {
-                "body": "user " + user + " user_array: " + user_array,
-                "msgtype": "m.text"
-            };
-            this.client.sendEvent(this.room, "m.room.message", user_info, "", (err:any, res:any) => {
-                console.log(err);
-            });
+            //const user_info = {
+            //    "body": "user " + user + " user_array: " + user_array,
+            //    "msgtype": "m.text"
+            //};
+            //this.client.sendEvent(this.room, "m.room.message", user_info, "", (err:any, res:any) => {
+            //    console.log(err);
+            //});
 
         }).catch((err: any) => {
             console.log("error logging in user " + user);
@@ -429,8 +503,8 @@ class Game
             this.loginStatus!.dispose(false, true);
 
             // create self user object
-            //this.userObj = new User(this.user);
-            console.log("head pos: " + this.xrCamera?.position + " left controller: " + this.leftController + " right controller: " + this.rightController);
+            this.userObj = new User(this.user);
+            //console.log("head pos: " + this.xrCamera?.position + " left controller: " + this.leftController + " right controller: " + this.rightController);
         });
 
         // add message listener to room - don't listen to messages in other rooms
@@ -442,6 +516,15 @@ class Game
                 // send messages to function to check if it's an update message
                 if (event.event.type == 'm.room.message') {
                     this.updateEnv(event.event.content.body);
+                    console.log("body: " + event.event.content.body);
+                    var body = JSON.parse(event.event.content.body);
+
+                    if (body.status == "update") {
+                        console.log("update type, print content: " + body.content);
+                        var obj = JSON.parse(body.content);
+                        console.log("print user: " + obj.id);
+                        console.log("print hpos: " + obj.hpos);
+                    }
                 }
             }
         });
@@ -464,37 +547,63 @@ class Game
 
 
 
-// used for storing and sharing the environment state...maybe
-class State {
-    // still....working out how to communicate these
-    //private users: Vector3[][]; // list of users in terms of position (headset/left/right)
-    //private objects: AbstractMesh[] = []; // list of environment objects (shape/options/position...etc?)
-    constructor() {
-    }
+// used for storing and sharing the environment state
+//class State {
 
-    public toString() : string {
-        return "";
-    }
-}
+//    //// all users in environment, mapped to their usernames
+//    //private users: Map<string, Object>;
 
-// user class - possible way to represent users
+//    //// all objects in environment, mapped to their names
+//    //private items: Map<string, AbstractMesh>;
+
+//    // should theoretically contain both users and items
+//    private objects: Map<string, Object>;
+
+//    constructor() {
+//        //users = new Map();
+//        //items = new Map();
+//        this.objects = new Map();
+//    }
+
+//    // receive update for an object passed as a JSON string
+//    receiveUpdate(info: string) {
+//        //var parsedInfo = JSON.parse(info);
+//        //var foundItem = this.objects.get(info.id);
+//        //if (foundItem) {
+//        //    foundItem.update(parsedInfo.info);
+//        //} else {
+//        //    var objInfo = JSON.parse(parsedInfo.info);
+//        //    console.log("num keys for object: " + Object.keys(objInfo).length);
+//        //    if (Object.keys(objInfo).length == 4) {
+//        //        this.objects.add(new User(info.id))
+//        //    } else {
+//        //        this.objects.add(new Item(info.id, parsedInfo.info));
+//        //    }
+//    }
+
+//    sendUpdate(id: string) {
+
+//    }
+
+//    //public toString() : string {
+//    //    return "";
+//    //}
+//}
+
+// represent users
 class User {
 
     // username
     private user: string;
-    private client: any;
-    private room: any;
 
     // visualize headset and controllers 
     private head: AbstractMesh;
     private left: AbstractMesh;
     private right: AbstractMesh;
 
-
-    constructor(user: string, client: any, head: Vector3 = new Vector3(0, 0, 0), headRotation: Vector3 = new Vector3(0, 0, 0),
+    constructor(user: string, head: Vector3 = new Vector3(0, 0, 0), headRotation: Vector3 = new Vector3(0, 0, 0),
                                             left: Vector3 = new Vector3(0, 0, 0), right: Vector3 = new Vector3(0, 0, 0)) {
         this.user = user;
-        this.client = client;
         this.head = MeshBuilder.CreateBox((user + "_head"), { size: 0.3 });
         this.head.position = head;
         this.head.rotation = headRotation;
@@ -502,6 +611,8 @@ class User {
         this.left.position = left;
         this.right = MeshBuilder.CreateSphere((user + "_right"), { segments: 16, diameter: 0.1 });
         this.right.position = right;
+
+        this.move(head, headRotation, left, right);
     }
 
     public move(head: Vector3, headRotation: Vector3, left: Vector3, right: Vector3) {
@@ -512,50 +623,75 @@ class User {
 
         var content = {
             type: "update",
-            content: JSON.stringify(this.toString())
+            content: this.toString()
         };
-        this.client.sendEvent(this.room, "m.room.message", content, "", (err: any, res: any) => {
-            console.log(err);
-        });
+
+        Messages.sendMessage(false, JSON.stringify(content));
     }
 
     public remove() {
         this.head.dispose();
         this.left.dispose();
         this.right.dispose();
+
+        var content = {
+            type: "remove",
+            content: this.user
+        };
+
+        Messages.sendMessage(false, JSON.stringify(content));
+    }
+
+    public update(info: string) {
+        var obj = JSON.parse(info);
+
+        Object.assign(this.head.position, obj.hpos);
+        Object.assign(this.head.rotation, obj.hrot);
+        Object.assign(this.left.position, obj.lpos);
+        Object.assign(this.right.position, obj.rpos);
     }
 
     // return JSON string
     public toString() {
         var ret = {
-            user: this.user,
-            hpos: this.head.position.clone(),
-            hrot: this.head.rotation.clone(),
-            lpos: this.left.position.clone(),
-            rpos: this.left.position.clone()
+            id: this.user,
+            info: {
+                hpos: this.head.position.clone(),
+                hrot: this.head.rotation.clone(),
+                lpos: this.left.position.clone(),
+                rpos: this.left.position.clone()
+            }
         };
 
-        return ret;
+        return JSON.stringify(ret);
     }
 }
 
 
-//class Item {
+class Item {
 
-//    private id: string;
-//    private mesh: AbstractMesh;
+    //private id: string;
+    ////private mesh: AbstractMesh;
 
-//    // pass in ID and options for meshbuilder
-//    constructor(id: string, opts: string) {
-//        this.id = id;
-//        var type = id.split("_")[0];
-//        switch (type) {
-//            case "box":
-//                this.mesh = MeshBuilder.CreateBox(id, )
-//        }
-//    }
-//}
+    //// pass in ID and options for meshbuilder
+    ////constructor(id: string, opts: string) {
+    ////    this.id = id;
+    ////    var type = id.split("_")[0];
+    ////    //switch (type) {
+    ////    //    case "box":
+    ////    //        this.mesh = MeshBuilder.CreateBox(id, )
+    ////    //}
+    ////}
 
+
+    //constructor(createInfo: string) {
+    //    var parsedInfo: 4
+    //}
+
+    //public update(info: string) {
+
+    //}
+}
 
 // start the game
 var game = new Game();
