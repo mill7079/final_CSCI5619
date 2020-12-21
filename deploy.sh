828e47939c74c3e0985ad666618cad8a7b21141d
Allison Miller <mill7079@umn.edu>
2020-12-20 18:37:26 -0700

    change deploy script machine

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 USER="mill7079"
-MACHINE="csel-kh4250-02.cselabs.umn.edu"
+MACHINE="csel-kh4250-08.cselabs.umn.edu"
 DIRECTORY=".www/Final/"
 
 rsync -avr --delete dist/ "$USER"@"$MACHINE":"$DIRECTORY"
